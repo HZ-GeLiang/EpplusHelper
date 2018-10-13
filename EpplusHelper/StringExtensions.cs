f34783caede48e89a59a096737b42d5e6e883e25
GeLiang <childliang@hotmail.com>
2018-10-13 13:27:47 +0800

    epplusHelper源码

---
+++ EpplusHelper/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpplusHelper
+{
+    public static class StringExtensions
+    {
+        public static string RemovePrefix(this string value, string prefix)
+        {
+            if (value == null) return null;
+            if (prefix == null || prefix.Length <= 0) return value;
+            return value.StartsWith(prefix) ? value.Substring(prefix.Length, value.Length - prefix.Length) : value;
+        }
+    }
+}
