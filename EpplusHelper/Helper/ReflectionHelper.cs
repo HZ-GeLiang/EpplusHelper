007716a5315800dee339c0c0682ed804d701ab79
GeLiang <childliang@hotmail.com>
2018-11-10 12:59:02 +0800

    新增枚举自定义提示

---
+++ EpplusHelper/Helper/ReflectionHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Reflection;
+
+namespace EpplusExtensions.Helper
+{
+    public class ReflectionHelper
+    {
+        public static object[] GetAttributeForProperty<T, TAttribute>(string propertyName)
+        {
+            if (propertyName == null) throw new ArgumentNullException(nameof(propertyName));
+            var pi = GetProperties<T>();
+            return GetProperty(pi, propertyName).GetCustomAttributes(typeof(TAttribute), false);
+        }
+
+        public static object[] GetAttributeForProperty<TAttribute>(Type propertyType, string propertyName)
+        {
+            if (propertyName == null) throw new ArgumentNullException(nameof(propertyName));
+            var pi = GetProperties(propertyType);
+            return GetProperty(pi, propertyName).GetCustomAttributes(typeof(TAttribute), false);
+        }
+
+        public static PropertyInfo[] GetProperties<T>()
+        {
+            Type type = typeof(T);
+            return GetProperties(type);
+        }
+
+
+        public static PropertyInfo[] GetProperties(Type type)
+        {
+            return type.GetProperties();
+        }
+        public static PropertyInfo GetProperty(PropertyInfo[] properties, string propertyName)
+        {
+            return GetProperty(properties, propertyName, false);
+        }
+        public static PropertyInfo GetProperty(PropertyInfo[] properties, string propertyName, bool notFindReturnNull)
+        {
+            if (properties == null) throw new ArgumentNullException(nameof(properties));
+            foreach (var prop in properties)
+            {
+                if (prop.Name != propertyName) continue;
+                return prop;
+            }
+            if (notFindReturnNull)
+            {
+                return null;
+            }
+            throw new ArgumentOutOfRangeException(nameof(propertyName));
+        }
+        public static object GetPropertyValue<T>(T model, string propertyName)
+        {
+            var pi = GetProperties<T>();
+            return GetProperty(pi, propertyName).GetValue(model);
+        }
+
+
+
+    }
+}
