12c22bac4e3b712cf67dc1a4d56992a6b2fe1ca8
GeLiang <childliang@hotmail.com>
2018-12-25 00:33:25 +0800

    打算往dotNetCore上迁移,一劳永逸

---
+++ SampleApp.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleApp.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
