dd8fa90aa15fd5c282f3280e699ee6cc5f93b35b
GeLiang <childliang@hotmail.com>
2019-01-07 17:49:36 +0800

    给普通的Excel在是使用前添加配置默认值

---
+++ SampleApp/Sample04_1.cs
@@ -0,0 +1,50 @@
+﻿using EpplusExtensions;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApp
+{
+    /// <summary>
+    /// 填充数据与数据源同步
+    /// </summary>
+    class Sample04_1
+    {
+        public void Run()
+        {
+
+            //string tempPath = $@"模版\各项导出模板.xlsx";
+            string tempPath = $@"C:\Users\child\Desktop\人事系统各项导出模板-修改.xlsx";
+            using (MemoryStream ms = new MemoryStream())
+            using (FileStream fs = System.IO.File.OpenRead(tempPath))
+            using (ExcelPackage excelPackage = new ExcelPackage(fs))
+            {
+                var dict = EpplusHelper.FillExcelDefaultConfig(excelPackage, new Dictionary<int, int>()
+                {
+                    {1,2},
+                    {2,2},
+                    {3,2},
+                    {4,2},
+                    {5,2},
+                    {6,1},
+                });
+                excelPackage.SaveAs(ms);
+                ms.Position = 0;
+                //ms.Save(@"模版\各项导出模板_Result.xlsx");
+                ms.Save($@"C:\Users\child\Desktop\11111.xlsx");
+                foreach (var item in dict)
+                {
+                    File.WriteAllText($@"模版\各项导出模板_Result_snippet_{item.Key}.txt", item.Value); //将字符串全部写入文件
+                }
+            }
+
+            System.Diagnostics.Process.Start(Path.GetDirectoryName(tempPath));
+        }
+
+    }
+}
---
+++ SampleApp/Sample04_2.cs
@@ -0,0 +1,28 @@
+﻿using EpplusExtensions;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApp
+{
+    /// <summary>
+    /// 填充数据与数据源同步
+    /// </summary>
+    class Sample04_2
+    {
+        public void Run()
+        {
+
+            string str = $@"序号	工号	姓名	性别	入司时间	转正时间	离职时间	离职类型	归属公司	部门	二级部门	三级部门	组别	岗位	岗位大类	行政职级	离职约谈记录
+          ";
+            var result = EpplusHelper.GetFillConfig(str);
+            Console.WriteLine(result);
+        }
+
+    }
+}
