1ca63e2f156d785f67dc30fbad6f34919636fca1
GeLiang <childliang@hotmail.com>
2019-01-07 17:55:37 +0800

    修改一些默认值

--- EpplusHelper/SheetBodyFillDataMethod.cs
+++ EpplusHelper/SheetBodyFillDataMethod.cs
@@ -1,11 +1,13 @@
-﻿namespace EpplusExtensions
+﻿using System;
+
+namespace EpplusExtensions
 {
     /// <summary>
     /// SheetBody数据填充方式
     /// </summary>
     public class SheetBodyFillDataMethod
     {
-        public SheetBodyFillDataMethodOption FillDataMethodOption = default(SheetBodyFillDataMethodOption);
+        public SheetBodyFillDataMethodOption FillDataMethodOption = SheetBodyFillDataMethodOption.Default;
 
         public SynchronizationDataSourceConfig SynchronizationDataSource = new SynchronizationDataSourceConfig();
 
@@ -16,11 +18,12 @@
         /// <summary>
         /// 按约定填充: 单元格上配置了什么就填充什么
         /// </summary>
-        Default = 0,
+        Default = 1,
         /// <summary>
         /// 在约定填充的基础上,数据源Datatable的列如果没有被填充使用,那么将自动填充
         /// </summary>
-        SynchronizationDataSource = 1
+        [Obsolete("不推荐使用,很多测试没通过")]
+        SynchronizationDataSource = 2
 
     }
 
