d318ae9b864c193c2ef83acc3c07da460c65bd2f
GeLiang <childliang@hotmail.com>
2019-01-30 11:20:34 +0800

    创建一个winform项目便于使用

---
+++ EPPlusHelperTool/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace EPPlusHelperTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Location = new System.Drawing.Point(22, 23);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(661, 49);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox1_DragDrop);
+            this.textBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBox1_DragEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "文件路径(文本框支持拖拽)";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(712, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 37);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "选择...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(25, 106);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "填充配置";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "自动初始化填充配置";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ EPPlusHelperTool/Form1.cs
@@ -0,0 +1,119 @@
+﻿using EpplusExtensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EPPlusHelperTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 弹出一个选择目录的对话框
+        /// </summary>
+        /// <returns></returns>
+        private string SelectPath()
+        {
+            FolderBrowserDialog path = new FolderBrowserDialog();
+            path.ShowDialog();
+            return path.SelectedPath;
+        }
+        /// <summary>
+        /// 弹出一个选择文件的对话框
+        /// </summary>
+        /// <returns></returns>
+        private string SelectFile(string filter = null)
+        {
+            //显示选择 文件对话框
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            //openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.InitialDirectory = System.AppDomain.CurrentDomain.BaseDirectory;
+            //openFileDialog1.Filter = "excel (*.xlsx)|*.xlsx";
+            if (filter != null)
+            {
+                openFileDialog1.Filter = filter;
+
+            }
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            var dialogResult = openFileDialog1.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                return openFileDialog1.FileName; //显示文件路径
+            }
+            else
+            {
+                return openFileDialog1.SafeFileName;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var selectfilePath = SelectFile("excel (*.xlsx)|*.xlsx");
+            if (selectfilePath.Length > 0)
+            {
+                this.textBox1.Text = selectfilePath;
+            }
+        }
+        private void textBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            string path = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+            this.textBox1.Text = path;
+        }
+        private void textBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Link;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string filePath = textBox1.Text.Trim();
+
+            //var fileName = Path.GetFileNameWithoutExtension(filePath);
+            //var suffix = Path.GetExtension(filePath);
+            var fileDir = Path.GetDirectoryName(filePath);
+            //Path.GetDirectoryName(Path.GetFullPath(tempPath))
+            //string filePathOut = Path.Combine(fileDir, $"{fileName}_result{suffix}");
+            //EpplusHelper.FillExcelDefaultConfig(filePath, filePathOut);
+            EpplusHelper.FillExcelDefaultConfig(filePath, fileDir);
+            OpenDirectory(fileDir);
+
+        }
+        /// <summary>
+        /// 打开文件目录
+        /// </summary>
+        /// <param name="filePath"></param>
+        private void OpenFileDirectory(string filePath)
+        {
+            MessageBox.Show($"文件已经生成,在'{filePath}'");
+            System.Diagnostics.Process.Start(Path.GetDirectoryName(filePath));
+        }
+        /// <summary>
+        /// 打开目录
+        /// </summary>
+        /// <param name="fileDirectoryName"></param>
+        private void OpenDirectory(string fileDirectoryName)
+        {
+            MessageBox.Show($"文件已经生成,在目录'{fileDirectoryName}'");
+            System.Diagnostics.Process.Start(fileDirectoryName);
+        }
+    }
+}
