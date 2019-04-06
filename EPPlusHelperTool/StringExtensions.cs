d2e5143ef56bc2eb5c8a4c5ee6ad884786220450
GeLiang <childliang@hotmail.com>
2019-04-06 17:43:54 +0800

    文件路径支持""包裹

---
+++ EPPlusHelperTool/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPPlusHelperTool
+{
+    internal static class StringExtensions
+    {
+        public static string 移除路径前后引号(this string filePath)
+        {
+            return filePath != null && filePath.Length > 2 && filePath[0] == '"' && filePath[filePath.Length - 1] == '"'
+                ? Path.GetDirectoryName(filePath.Substring(1, filePath.Length - 1 - 1))
+                : filePath;
+        }
+    }
+}
