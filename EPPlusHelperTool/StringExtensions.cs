4407c7ec9ec4a4aac330faec51e44e6f93ac65f7
GeLiang <childliang@hotmail.com>
2019-04-06 18:02:08 +0800

    新增一个功能"生成配置项"

--- EPPlusHelperTool/StringExtensions.cs
+++ EPPlusHelperTool/StringExtensions.cs
@@ -9,10 +9,14 @@ namespace EPPlusHelperTool
 {
     internal static class StringExtensions
     {
+        public static string GetDirectoryName(this string filePath)
+        {
+            return Path.GetDirectoryName(移除路径前后引号(filePath));
+        }
         public static string 移除路径前后引号(this string filePath)
         {
             return filePath != null && filePath.Length > 2 && filePath[0] == '"' && filePath[filePath.Length - 1] == '"'
-                ? Path.GetDirectoryName(filePath.Substring(1, filePath.Length - 1 - 1))
+                ? filePath.Substring(1, filePath.Length - 1 - 1)
                 : filePath;
         }
     }
