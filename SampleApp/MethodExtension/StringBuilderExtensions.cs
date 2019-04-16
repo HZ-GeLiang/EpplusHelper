5c6097117823f90c5ad7665cd47664f831762d88
GeLiang <childliang@hotmail.com>
2019-04-16 10:07:29 +0800

    修改命名空间

--- SampleApp/MethodExtension/StringBuilderExtensions.cs
+++ SampleApp/MethodExtension/StringBuilderExtensions.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 
-namespace SampleApp
+namespace SampleApp.MethodExtension
 {
     internal static class StringBuilderExtensions
     {
