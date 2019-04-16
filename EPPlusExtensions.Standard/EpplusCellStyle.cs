0906136865bcdb22191a4245293d3e0f30a6b0e5
GeLiang <childliang@hotmail.com>
2019-04-16 08:01:11 +0800

    去掉没有用过的代码

--- EPPlusExtensions.Standard/EpplusCellStyle.cs
+++ EPPlusExtensions.Standard/EpplusCellStyle.cs
@@ -3,20 +3,20 @@ using System.Drawing;
 
 namespace EPPlusExtensions
 {
-    /// <summary>
-    /// 由EPPlusHelper.SetCellStyle  EPPlusHelper.SetCellStyle推出来的
-    /// </summary>
-    public class EPPlusCellStyle
-    {
-        public ExcelHorizontalAlignment HorizontalAlignment { get; set; }
-        public ExcelVerticalAlignment VerticalAlignment { get; set; }
-        public bool WrapText { get; set; }
-        public bool FontBold { get; set; }
-        public Color FontColor { get; set; }
-        public Color BackgroundColor { get; set; }
-        public string FontName { get; set; }
-        public float FontSize { get; set; }
-        public ExcelFillStyle PatternType { get; set; }
-        public bool ShrinkToFit { get; set; }
-    }
+    ///// <summary>
+    ///// 由EPPlusHelper.SetCellStyle  EPPlusHelper.SetCellStyle推出来的
+    ///// </summary>
+    //public class EPPlusCellStyle
+    //{
+    //    public ExcelHorizontalAlignment HorizontalAlignment { get; set; }
+    //    public ExcelVerticalAlignment VerticalAlignment { get; set; }
+    //    public bool WrapText { get; set; }
+    //    public bool FontBold { get; set; }
+    //    public Color FontColor { get; set; }
+    //    public Color BackgroundColor { get; set; }
+    //    public string FontName { get; set; }
+    //    public float FontSize { get; set; }
+    //    public ExcelFillStyle PatternType { get; set; }
+    //    public bool ShrinkToFit { get; set; }
+    //}
 }
