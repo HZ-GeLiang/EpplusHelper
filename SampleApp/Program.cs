83c663b9d1849cae7f502a192321bb432286d5e8
GeLiang <childliang@hotmail.com>
2019-07-15 23:40:23 +0800

    修复膏bug:获得模版数据检测提示

--- SampleApp/Program.cs
+++ SampleApp/Program.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using EpplusExtensions;
+using EpplusExtensions.Attributes;
 using OfficeOpenXml;
 
 
@@ -13,10 +14,43 @@ namespace SampleApp
 {
     class Program
     {
+      
+
+        class Model1
+        {
+            public string 序号 { get; set; }
+            //public string 姓名 { get; set; }
+            [ExcelColumnIndex(3)]
+            [DisplayExcelColumnName("请假次数")]
+            public string 请假次数1 { get; set; }
+            [ExcelColumnIndex(4)]
+            [DisplayExcelColumnName("请假次数")]
+            public string 请假次数2 { get; set; }
+        }
         static void Main(string[] args)
         {
-            new Sample02_7().Run();
+            new Sample02_5().Run();
 
+
+            string filePath = $@"C:\Users\child\Desktop\Sample02_7.xlsx";
+            using (MemoryStream ms = new MemoryStream())
+            using (FileStream fs = new System.IO.FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (ExcelPackage excelPackage = new ExcelPackage(fs))
+            {
+                ExcelWorksheet ws = EpplusHelper.GetExcelWorksheet(excelPackage, 1);
+                List<Model1> list;
+                try
+                {
+                    list = EpplusHelper.GetList<Model1>(ws, 2);
+                    ObjectDumper.Write(list);
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+                Console.WriteLine("读取完毕");
+            }
+          
             //OpenDirectoryHelp.OpenFilePath(System.IO.Path.Combine(OpenDirectoryHelp.GetSaveFilePath(), @"Debug\模版\"));
             //new Sample01_1().Run();
             //new Sample01_2().Run();
