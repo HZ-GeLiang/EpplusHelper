529daf2da2f822ebec83989388f9b3f405ca39bf
GeLiang <childliang@hotmail.com>
2019-12-04 16:40:18 +0800

    代码示例签入

--- SampleApp/Sample01_1_2.cs
+++ SampleApp/Sample01_1_2.cs
@@ -20,25 +20,20 @@ namespace SampleApp
         {
             string filePath = @"模版\Sample01_1_2.xlsx";
             using (MemoryStream ms = new MemoryStream())
-            //using (FileStream fs = System.IO.File.OpenRead(filePath)) 
             using (FileStream fs = new System.IO.FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
             using (ExcelPackage excelPackage = new ExcelPackage(fs))
             {
                 var config = EPPlusHelper.GetEmptyConfig();
                 var configSource = EPPlusHelper.GetEmptyConfigSource();
                 EPPlusHelper.SetDefaultConfigFromExcel(excelPackage, config, 1);
-                var dtHead = GetDataTable_Head();
                 //EPPlusHelper.SetConfigSourceHead(configSource, dtHead, dtHead.Rows[0]);
                 //EPPlusHelper.SetConfigSourceHead(configSource, dtHead);
-                configSource.Head = dtHead;
-                configSource.Body[1].Option.DataSource = GetDataTable_Body();
- 
+                configSource.Head = Sample01_1.GetDataTable_Head();
+                configSource.Body[1].Option.DataSource = Sample01_1.GetDataTable_Body();
                 EPPlusHelper.FillData(excelPackage, config, configSource, "导出测试", 1);
-                var ws = EPPlusHelper.GetExcelWorksheet(excelPackage, "导出测试");
-                ws.Protection.IsProtected = true;
-                ws.Protection.AllowSelectLockedCells = false;
-                ws.Protection.AllowSelectUnlockedCells = true;
-                ws.Protection.SetPassword("123");
+
+
+                //删除的2种方式
                 //EPPlusHelper.DeleteWorksheet(excelPackage, 1);
                 EPPlusHelper.DeleteWorksheetAll(excelPackage, EPPlusHelper.FillDataWorkSheetNameList);
                 excelPackage.SaveAs(ms);
@@ -48,41 +43,6 @@ namespace SampleApp
             System.Diagnostics.Process.Start(Path.GetDirectoryName(filePath));
         }
 
-        static DataTable GetDataTable_Head()
-        {
-            DataTable dt = new DataTable();
-            dt.Columns.Add("Title");
 
-            DataRow dr = dt.NewRow();
-            dr["Title"] = "2018第一学期考试";
-            dt.Rows.Add(dr);
-            return dt;
-
-        }
-        static DataTable GetDataTable_Body()
-        {
-            DataTable dt = new DataTable();
-            dt.Columns.Add("Name");
-            dt.Columns.Add("Chinese");
-            dt.Columns.Add("Math");
-            dt.Columns.Add("English");
-
-            DataRow dr = dt.NewRow();
-            dr["Name"] = "张三";
-            dr["Chinese"] = 60;
-            dr["Math"] = 60.5;
-            dr["English"] = 61;
-            dt.Rows.Add(dr);
-
-            dr = dt.NewRow();
-            dr["Name"] = "李四";
-            dr["Chinese"] = 70;
-            dr["Math"] = 80.5;
-            dr["English"] = 91;
-            dt.Rows.Add(dr);
-
-            return dt;
-
-        }
     }
 }
