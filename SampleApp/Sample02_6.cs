9e4fc8675c853b60b3a2506746f06eb532d517d8
GeLiang <childliang@hotmail.com>
2019-12-04 16:45:14 +0800

    示例代码去掉无用代码

--- SampleApp/Sample02_6.cs
+++ SampleApp/Sample02_6.cs
@@ -18,7 +18,6 @@ namespace SampleApp
         public void Run()
         {
             string filePath = @"模版\Sample02_1.xlsx";
-            //using (FileStream fs = System.IO.File.OpenRead(filePath))
             using (FileStream fs = new System.IO.FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
             using (ExcelPackage excelPackage = new ExcelPackage(fs))
             {
