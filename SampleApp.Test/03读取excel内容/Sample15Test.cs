06f0e3732ddd728b3f9912f511273798f603d9b8
GeLiang <childliang@hotmail.com>
2019-12-25 10:39:08 +0800

    添加示例

---
+++ SampleApp.Test/03读取excel内容/Sample15Test.cs
@@ -0,0 +1,28 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EPPlusExtensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SampleApp._03读取excel内容;
+
+namespace SampleApp.Test._03读取excel内容
+{
+    [TestClass]
+    public class Sample15Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var excelList = Sample15.Run();
+            var resultList = excelList.GetEmpty().ToList();
+            resultList.Add(new Sample15.ExcelModel { Num1 = 1, Num2 = 2, Sum = 3, CopyNum1 = 1, CopySum = 3 });
+            resultList.Add(new Sample15.ExcelModel { Num1 = 2, Num2 = 3, Sum = 5, CopyNum1 = 2, CopySum = 5 });
+            CollectionAssert.AreEqual(excelList, resultList);
+        }
+    }
+}
---
+++ SampleApp/03读取excel内容/Sample15.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EPPlusExtensions;
+using OfficeOpenXml;
+using SampleApp._01填充数据;
+using SampleApp.MethodExtension;
+
+namespace SampleApp._03读取excel内容
+{
+    public class Sample15
+    {
+        public static List<ExcelModel> Run()
+        {
+            string filePath = @"模版\03读取excel内容\Sample15.xlsx";
+            using (var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (var excelPackage = new ExcelPackage(fs))
+            {
+                var ws = EPPlusHelper.GetExcelWorksheet(excelPackage, 1);
+                var args = EPPlusHelper.GetExcelListArgsDefault<ExcelModel>(ws, 2);
+                //args.ScanLine = ScanLine.MergeLine;//默认的
+                var list = EPPlusHelper.GetList(args);
+                ObjectDumper.Write(list);
+                Console.WriteLine("读取完毕");
+                return list;
+            }
+        }
+
+
+        public class ExcelModel
+        {
+            public int Num1 { get; set; }
+            public int Num2 { get; set; }
+            public int Sum { get; set; }
+            public int CopyNum1 { get; set; }
+            public int CopySum { get; set; }
+             
+            public override bool Equals(object obj)
+            {
+                if (obj == null || !obj.GetType().Equals(this.GetType()))
+                {
+                    return false;
+                }
+
+                ExcelModel y = (ExcelModel)obj;
+
+                return this.Num1 == y.Num1 &&
+                       this.Num2 == y.Num2 &&
+                       this.Sum == y.Sum &&
+                       this.CopyNum1 == y.CopyNum1 &&
+                       this.CopySum == y.CopySum;
+            }
+
+            //重写Equals方法必须重写GetHashCode方法，否则发生警告
+            public override int GetHashCode()
+            {
+                return this.Num1.GetHashCode() +
+                       this.Num2.GetHashCode() +
+                       this.Sum.GetHashCode() +
+                       this.CopyNum1.GetHashCode() +
+                       this.CopySum.GetHashCode();
+            }
+        }
+    }
+}
