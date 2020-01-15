d8962629ae56722f1e2d537e71b2fad59f0d8717
GeLiang <childliang@hotmail.com>
2020-01-15 11:15:05 +0800

    重命名KVSource类型名称,编译不过去的问题

---
+++ EPPlusExtensions.Standard/Cache/GenericCache.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Text;
+
+namespace EPPlusExtensions.Cache
+{
+
+    //public class GenericCache<T>
+    //{
+    //    static GenericCache()
+    //    {
+    //        _CachedValue = string.Format("{0}_{1}",
+    //            typeof(T).FullName, DateTime.Now.ToString("yyyyMMddHHmmss.fff"));
+    //    }
+
+    //    private static string _CachedValue = "";
+
+    //    public static string GetCache()
+    //    {
+    //        return _CachedValue;
+    //    }
+    //}
+
+
+}
