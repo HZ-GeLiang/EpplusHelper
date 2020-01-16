1a5093dc7b4e9457752249751949591a0f82a414
GeLiang <childliang@hotmail.com>
2020-01-16 18:55:25 +0800

    删除无用的if分支

--- SampleApp.Test/03读取excel内容/Sample14Test.cs
+++ SampleApp.Test/03读取excel内容/Sample14Test.cs
@@ -26,8 +26,9 @@ namespace SampleApp.Test._03读取excel内容
             }
             catch (Exception ex)
             {
-                Assert.AreEqual(ex.Message, @"检测到数据的起始列是合并行,请确保当前行的数据都是合并行.当前C2单元格不满足需求");
+                Assert.AreEqual(ex.Message, @"检测到数据的起始列是合并行,请确保当前行的数据都是合并行.当前C2单元格不满足需求.");
             } 
         }
     }
 }
+ 
