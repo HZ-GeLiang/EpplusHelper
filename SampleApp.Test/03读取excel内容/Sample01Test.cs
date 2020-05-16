9f66409248776d478cbc8f39e5f04bcb402a7be0
GeLiang <childliang@hotmail.com>
2020-05-16 10:45:53 +0800

    修改单元测试

--- SampleApp.Test/03读取excel内容/Sample01Test.cs
+++ SampleApp.Test/03读取excel内容/Sample01Test.cs
@@ -12,12 +12,12 @@ namespace SampleApp.Test._03读取excel内容
         {
             var excelList = Sample01.Run();
             var resultList = excelList.GetEmpty().ToList();
-            resultList.Add(new Sample01.ExcelModel { 序号 = "1", 部门 = "事业1部", 部门负责人 = "赵六", 部门负责人确认签字 = "娃娃" });
-            resultList.Add(new Sample01.ExcelModel { 序号 = "2", 部门 = "事业2部", 部门负责人 = "赵六", 部门负责人确认签字 = "菲菲" });
-            resultList.Add(new Sample01.ExcelModel { 序号 = "3", 部门 = "事业3部", 部门负责人 = "王五", 部门负责人确认签字 = "佩琪" });
-            resultList.Add(new Sample01.ExcelModel { 序号 = "4", 部门 = "事业4部", 部门负责人 = "jam", 部门负责人确认签字 = "jam" });
-            resultList.Add(new Sample01.ExcelModel { 序号 = "4", 部门 = "事业4部", 部门负责人 = "jam", 部门负责人确认签字 = "jam" });
-            resultList.Add(new Sample01.ExcelModel { 序号 = "6", 部门 = "事业6部", 部门负责人 = "jack", 部门负责人确认签字 = "jack" });
+            resultList.Add(new Sample01.ExcelModel { 序号 = 1, 部门 = "事业1部", 部门负责人 = "赵六", 部门负责人确认签字 = "娃娃" });
+            resultList.Add(new Sample01.ExcelModel { 序号 = 2, 部门 = "事业2部", 部门负责人 = "赵六", 部门负责人确认签字 = "菲菲" });
+            resultList.Add(new Sample01.ExcelModel { 序号 = 3, 部门 = "事业3部", 部门负责人 = "王五", 部门负责人确认签字 = "佩琪" });
+            resultList.Add(new Sample01.ExcelModel { 序号 = 4, 部门 = "事业4部", 部门负责人 = "jam", 部门负责人确认签字 = "jam" });
+            resultList.Add(new Sample01.ExcelModel { 序号 = 4, 部门 = "事业4部", 部门负责人 = "jam", 部门负责人确认签字 = "jam" });
+            resultList.Add(new Sample01.ExcelModel { 序号 = 6, 部门 = "事业6部", 部门负责人 = "jack", 部门负责人确认签字 = "jack" });
             CollectionAssert.AreEqual(excelList, resultList);
         }
     }
--- SampleApp.Test/03读取excel内容/Sample12Test.cs
+++ SampleApp.Test/03读取excel内容/Sample12Test.cs
@@ -11,7 +11,7 @@ namespace SampleApp.Test._03读取excel内容
         [TestMethod]
         public void TestMethod1()
         {
-            var excelList = Sample12.Run<Sample12.ExcelModel>();
+            var excelList = Sample12.Run<Sample12.ExcelModel>(true);
             var resultList = excelList.GetEmpty().ToList();
             resultList.Add(new Sample12.ExcelModel { 序号 = 1, 姓名 = "娃娃", JanuaryStatistics = 4, FebruaryStatistics = 7 });
             resultList.Add(new Sample12.ExcelModel { 序号 = 2, 姓名 = "菲菲", JanuaryStatistics = 5, FebruaryStatistics = 8 });
@@ -22,18 +22,29 @@ namespace SampleApp.Test._03读取excel内容
         [TestMethod]
         public void 应该抛异常()
         {
-            Assert.ThrowsException<Exception>(() => Sample12.Run<Sample12.ExcelModel2>());
+            Assert.ThrowsException<Exception>(() => Sample12.Run<Sample12.ExcelModel2>(true));
 
             try
             {
-                Sample12.Run<Sample12.ExcelModel2>();
+                Sample12.Run<Sample12.ExcelModel2>(true);
             }
             catch (Exception ex)
             {
                 Assert.AreEqual(ex.Message, $@"程序报错:Message:无效的数字
 参数名: 姓名(B列)");
             }
+            try
+            {
+                Sample12.Run<Sample12.ExcelModel2>(false);
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual(ex.Message, $@"程序报错:Message:无效的数字
+参数名: 姓名(B3,B4,B5)");
+            }
 
         }
+
+
     }
 }
