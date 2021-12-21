50dc9578f2b5fe1f84863f73fb7ea86a8b63432c
GeLiang <childliang@hotmail.com>
2021-12-21 11:16:50 +0800

    GetFillDefaultConfig添加Alias

--- SampleApp/05自动初始化填充配置/Sample01.cs
+++ SampleApp/05自动初始化填充配置/Sample01.cs
@@ -1,5 +1,6 @@
 ﻿using EPPlusExtensions;
 using System;
+using System.Collections.Generic;
 
 namespace SampleApp._05自动初始化填充配置
 {
@@ -8,7 +9,21 @@ namespace SampleApp._05自动初始化填充配置
         public static string Run()
         {
             var result = EPPlusHelper.GetFillDefaultConfig("序号	工号	姓名	性别");
-            Console.WriteLine(result);
+            Console.WriteLine(result); //"$tb1序号	$tb1工号	$tb1姓名	$tb1性别";
+            return result;
+        }
+
+    }
+    public class Sample01_alias
+    {
+        public static string Run()
+        {
+            var alias = new Dictionary<string, string>()
+            {
+                {"序号" ,"Index" }
+            };
+            var result = EPPlusHelper.GetFillDefaultConfig("序号	工号	姓名	性别", alias: alias);
+            Console.WriteLine(result); //"$tb1Index	$tb1工号	$tb1姓名	$tb1性别";
             return result;
         }
 
