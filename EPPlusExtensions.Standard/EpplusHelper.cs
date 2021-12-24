fe42d5df1e0e9c62e11bcb3e965568584a91d320
GeLiang <childliang@hotmail.com>
2021-12-24 11:29:37 +0800

    封装读取文件的简便方法

--- EPPlusExtensions.Standard/EpplusHelper.cs
+++ EPPlusExtensions.Standard/EpplusHelper.cs
@@ -4253,5 +4253,21 @@ namespace EPPlusExtensions
         }
 
         #endregion
+
+        /// <summary>
+        /// 读取一个文件,获得一个文件流
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="mode"></param>
+        /// <param name="access"></param>
+        /// <param name="share"></param>
+        /// <returns></returns>
+        public static FileStream GetFileStream(string filePath,
+            FileMode mode = FileMode.Open,
+            FileAccess access = FileAccess.Read,
+            FileShare share = FileShare.ReadWrite)
+        {
+            return new FileStream(filePath, mode, access, share);
+        }
     }
 }
