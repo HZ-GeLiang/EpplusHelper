e5745a3042e956be6a09dcd5e7060d4966ae2ff4
GeLiang <gel@ccf.com.cn>
2022-01-20 15:45:05 +0800

    签入

--- EPPlusExtensions.Standard/ExcelCellPoint.cs
+++ EPPlusExtensions.Standard/ExcelCellPoint.cs
@@ -107,12 +107,12 @@ namespace EPPlusExtensions
             return sum;
         }
 
-        private static Dictionary<int, char> r1C1_reverse_dict = new Dictionary<int, char>
+        private static Dictionary<int, char> r1C1_reverse_dict = new Dictionary<int, char>(27)
         {
             {01,'A'},{02,'B'},{03,'C'},{04,'D'},{05,'E'},{06,'F'},{07,'G'},
             {08,'H'},{09,'I'},{10,'J'},{11,'K'},{12,'L'},{13,'M'},{14,'N'},
             {15,'O'},{16,'P'},{17,'Q'},{18,'R'},{19,'S'},{20,'T'},{21,'U'},
-            {22,'V'},{23,'W'},{24,'X'},{25,'Y'},{26,'Z'},{0,'A'}
+            {22,'V'},{23,'W'},{24,'X'},{25,'Y'},{26,'Z'},{00,'A'}
         };
 
         /// <summary>
