d76275b2fa48ac2a20be5cd9bbd62a26d6a8389b
GeLiang <childliang@hotmail.com>
2024-09-06 09:43:14 +0800

    签入

--- EPPlusExtensions.Standard/EpplusHelper.cs
+++ EPPlusExtensions.Standard/EpplusHelper.cs
@@ -21,8 +21,8 @@ namespace EPPlusExtensions
         public static List<string> FillDataWorkSheetNameList = new List<string>();
 
         //类型参考网址:
-        //网址1:http://filext.com/faq/office_mime_types.php
-        //网址2:https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
+        //网址1: http://filext.com/faq/office_mime_types.php
+        //网址2: https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
         public const string XlsxContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
 
         #region GetExcelWorksheet
